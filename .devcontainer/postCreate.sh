d73315a4686c5627c529eb88dd8d45bedcf4b0fc
Gabriel Brito de França <gabrielbfranca@gmail.com>
2024-09-05 02:20:44 +0000

    arrumando o repositório

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,8 @@
 cd /workspaces/bsn &&\
     rosdep install --from-paths src --ignore-src -r -y &&\
+    rm -rf build &&\
+    rm -rf devel &&\
     catkin_make
     echo "source /workspaces/bsn/devel/setup.sh" >> /root/.bashrc
+    source /opt/ros/melodic/setup.bash
+

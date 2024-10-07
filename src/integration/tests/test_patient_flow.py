e3fb1a0470078367c2207fc1c7b767fe73a49f44
Gabriel Brito de França <gabrielbfranca@gmail.com>
2024-10-07 12:46:01 +0000

    unverified charac test

---
+++ src/integration/tests/test_patient_flow.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import rospy
+import unittest
+import rostest
+from std_srvs.srv import Empty
+from services.srv import PatientData
+from std_msgs.msg import String
+
+class TestPatientFlow(unittest.TestCase):
+    def setUp(self):
+        rospy.init_node('test_patient_flow')
+        # Subscribe to g4t1 topic
+        self.g4t1_data = None
+        rospy.Subscriber("/g4t1", String, self.g4t1_callback)
+        
+        # Wait for PatientData service
+        rospy.wait_for_service('/Patient/getPatientData')
+        self.patient_service = rospy.ServiceProxy('/Patient/getPatientData', PatientData)
+
+    def g4t1_callback(self, msg):
+        self.g4t1_data = msg.data
+
+    def test_patient_data_flow(self):
+        # Call the PatientData service with a mock request
+        request = PatientData.Request()
+        request.vitalSign = "heart_rate"  # Example vital sign request
+        response = self.patient_service(request)
+        
+        # Assert we got data from the service
+        self.assertIsNotNone(response, "No response from PatientData service")
+        
+        # Give G4T1 time to process and publish data
+        rospy.sleep(2)
+
+        # Check that g4t1 node published processed data
+        self.assertIsNotNone(self.g4t1_data, "No data published on /g4t1 topic")
+        self.assertIn("risk", self.g4t1_data, "Risk data not correctly processed")
+
+if __name__ == '__main__':
+    rostest.rosrun('integration', 'test_patient_flow', TestPatientFlow)

b63cdb5e0e17e60d695ee20da3ccd8fa507dda5c
Gabriel Brito de França <gabrielbfranca@gmail.com>
2025-01-22 21:23:04 +0000

    advances in fixing the issue, component::setUp freeze

--- src/sa-bsn/target_system/components/component/test/test_Sensor.cpp
+++ src/sa-bsn/target_system/components/component/test/test_Sensor.cpp
@@ -155,9 +155,9 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "test_g3t1_3");
     // ros::NodeHandle nh;
     return RUN_ALL_TESTS();
-}*/
+}
 #include <gtest/gtest.h>
-#include "component/g3t1_3/G3T1_3.hpp"
+#include "G3T1_3.hpp"
 #include "ros/ros.h"
 #include "std_srvs/SetBool.h"
 #include "services/PatientData.h"
@@ -238,7 +238,7 @@ protected:
 {
     EXPECT_NO_THROW(sensor->setUp());
     EXPECT_NO_THROW(sensor->tearDown());
-}*/
+}
 TEST_F(G3T1_3Fixture, TestGetPatientData)
 {
     ros::NodeHandle nh;
@@ -300,4 +300,140 @@ int main(int argc, char **argv)
 
     ros::shutdown(); // Ensure ROS is properly shut down
     return result;
+}
+*/
+#include "component/g3t1_3/G3T1_3.hpp"
+#include <gtest/gtest.h>
+#include <ros/ros.h>
+#include <std_srvs/SetBool.h>
+#include <messages/SensorData.h>
+#include <services/PatientData.h>
+#include <unistd.h>
+
+void dummyCallback(const ros::MessageEvent<archlib::Event const> &event)
+{
+    // Dummy function that does nothing but ensures subscribers exist
+}
+class G3T1_3Tests : public ::testing::Test
+{
+protected:
+    G3T1_3 *sensor_node;
+
+    // Override the default SetUp function
+    void SetUp() override
+    {
+        ros::param::set("/temperature_LowRisk", "36.0,37.0");
+        ros::param::set("/temperature_MidRisk0", "37.1,38.0");
+        ros::param::set("/temperature_HighRisk0", "38.1,39.0");
+        ros::param::set("/temperature_MidRisk1", "39.1,40.0");
+        ros::param::set("/temperature_HighRisk1", "40.1,41.0");
+
+        ros::param::set("/lowrisk", "0,20");
+        ros::param::set("/midrisk", "21,65");
+        ros::param::set("/highrisk", "66,100");
+
+        ros::param::set("/instant_recharge", true);
+        ros::param::set("/start", true);
+
+        int argc = 0;
+        char **argv = nullptr;
+        sensor_node = new G3T1_3(argc, argv, "test_g3t1_3");
+        ROS_INFO("g3t1 initialized");
+        // Mock subscribers to prevent blocking in Component::setUp
+        ros::NodeHandle nh;
+        nh.subscribe<archlib::Event>("collect_event", 10, &dummyCallback);                // Corrected type: archlib::Event
+        nh.subscribe<archlib::Status>("collect_status", 10, &dummyCallback);              // Corrected type: archlib::Status
+        nh.subscribe<archlib::EnergyStatus>("collect_energy_status", 10, &dummyCallback); // Corrected type: archlib::EnergyStatus
+
+        // Allow some time for subscribers to connect
+        ros::spinOnce(); // Process any pending messages
+        usleep(1000);    // Allow time for subscribers to connect
+
+        sensor_node->setUp(); // Initialize the node
+        ROS_INFO("g3t1 setup");
+    }
+
+    // Tear down resources
+    void TearDown() override
+    {
+        sensor_node->tearDown(); // Clean up the node
+        delete sensor_node;
+    }
+    bool mockPatientDataService(services::PatientData::Request &req, services::PatientData::Response &res)
+    {
+        if (req.vitalSign == "temperature")
+        {
+            res.data = 37.5; // Simulated patient temperature
+            return true;
+        }
+        return false;
+    }
+};
+/*
+TEST_F(G3T1_3Tests, TestCollectPatientData)
+{
+    ros::NodeHandle nh;
+
+    // Mock the services/PatientData service
+    auto patient_data_srv = nh.advertiseService<services::PatientData::Request, services::PatientData::Response>(
+        "getPatientData",
+        [this](services::PatientData::Request &req, services::PatientData::Response &res)
+        {
+            return this->mockPatientDataService(req, res);
+        });
+
+    ros::spinOnce();
+
+    // Call the `collect` method of the G3T1_3 node
+    double collected_data = sensor_node->collect();
+
+    // Verify the result
+    EXPECT_DOUBLE_EQ(collected_data, 37.5); // Assert that the collected data matches the mock response
+}
+*/
+TEST_F(G3T1_3Tests, TestProcess)
+{
+    ROS_INFO("in testCASE");
+    double input_data = 36.5;
+    double processed_data = sensor_node->process(input_data);
+    EXPECT_NEAR(processed_data, input_data, 0.1); // Verify processed data is close to input
+}
+/*
+TEST_F(G3T1_3Tests, TestTransfer)
+{
+    ros::NodeHandle nh;
+    auto sensor_pub = nh.advertise<messages::SensorData>("thermometer_data", 10);
+
+    double input_data = 36.5;
+    ASSERT_NO_THROW(sensor_node->transfer(input_data));
+
+    // Sleep to allow message propagation
+    usleep(10000);
+    ros::spinOnce();
+
+    // Here, you can use a subscriber to verify the message was published correctly if needed
+}
+
+TEST_F(G3T1_3Tests, TestRiskLabeling)
+{
+    double risk_low = 10.0;
+    double risk_mid = 50.0;
+    double risk_high = 90.0;
+
+    std::string low_label = sensor_node->label(risk_low);
+    EXPECT_EQ(low_label, "low");
+
+    std::string mid_label = sensor_node->label(risk_mid);
+    EXPECT_EQ(mid_label, "moderate");
+
+    std::string high_label = sensor_node->label(risk_high);
+    EXPECT_EQ(high_label, "high");
+}
+*/
+// Main function to run all the tests
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "test_g3t1_3");
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
